7e29e15919f7bde85446cf8672f122820f62086e
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-02-21 14:40:27 +0900

    FEAT: elseif.js 작성

---
+++ elseif.js
@@ -0,0 +1,12 @@
+// else if 피하기 & else 피하기
+// ex
+
+const x = 1;
+
+if (x >= 1) {
+  console.log("x는 0과 같거나 크다");
+} else if (x > 0) {
+  console.log("x는 0보다 크다");
+} else {
+  console.log("Else");
+}
